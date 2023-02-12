0509d997cb1e45add97fe423c97149a3d5e9514b
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-13 00:23:20 +0100

    JavaFX program!

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -1,7 +1,28 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+
     }
 }
