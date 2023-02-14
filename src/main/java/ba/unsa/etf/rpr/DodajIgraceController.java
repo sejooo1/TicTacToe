e3f1e29db0641fbffe71e81349595234d0645866
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-15 00:51:56 +0100

    Dodan DodajIgraceController!

---
+++ src/main/java/ba/unsa/etf/rpr/DodajIgraceController.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DodajIgraceController {
+    @FXML
+    public void handleNazadClick(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Scene newScene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(newScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
