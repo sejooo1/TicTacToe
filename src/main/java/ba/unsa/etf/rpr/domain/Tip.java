cb9144ad796352e966affff1002d6a6f2697b594
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-15 22:10:13 +0100

    Dodana klasa Tip!

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Tip.java
@@ -0,0 +1,51 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Tip implements Idable{
+    private int id;
+    private String opis;
+
+    public Tip(int id, String opis) {
+        this.id = id;
+        this.opis = opis;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tip tip)) return false;
+        return getId() == tip.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Tip{" +
+                "id=" + id +
+                ", opis='" + opis + '\'' +
+                '}';
+    }
+}
