f446c0dd5235ecb50844775adac36e1f11e9975e
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-17 18:38:54 +0100

    Implementirana FactoryDAO klasa!

--- src/main/java/ba/unsa/etf/rpr/dao/FactoryDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/FactoryDAO.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.dao;
 
 public class FactoryDAO {
+    private static final IgracDAO igracDAO = IgracDAOSql.getInstance();
+    private static final MecDAO mecDAO = MecDAOSql.getInstance();
+    private static final TipDAO tipDAO = TipDAOSql.getInstance();
+    private FactoryDAO(){
+    }
+
+    public static IgracDAO igracDAO(){
+        return igracDAO;
+    }
+
+    public static MecDAO mecDAO(){
+        return mecDAO;
+    }
+
+    public static TipDAO tipDAO(){
+        return tipDAO;
+    }
 }
