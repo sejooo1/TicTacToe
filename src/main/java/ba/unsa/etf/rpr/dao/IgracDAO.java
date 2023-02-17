ac4095a2c651cecb818eda889f1b100ffe2cff23
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-17 17:13:20 +0100

    Ispravka u IgracDAO interfejsu!

--- src/main/java/ba/unsa/etf/rpr/dao/IgracDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/IgracDAO.java
@@ -3,9 +3,8 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Igrac;
 import ba.unsa.etf.rpr.exceptions.MojException;
 
-import java.util.List;
 
 public interface IgracDAO extends DAO<Igrac>{
 
-    List<Igrac> dajPoImenu(String ime) throws MojException;
+    Igrac dajPoImenu(String ime) throws MojException;
 }
