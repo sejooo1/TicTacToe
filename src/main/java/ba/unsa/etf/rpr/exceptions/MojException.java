e510cdbdd15113ada296b4a56daf4c519803ad86
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:43:11 +0100

    JavaDoc za MojException!

--- src/main/java/ba/unsa/etf/rpr/exceptions/MojException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MojException.java
@@ -1,11 +1,25 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * Custom exception MojException.
+ */
 public class MojException extends Exception{
 
+    /**
+     * Instantiates a new Moj exception.
+     *
+     * @param msg    the msg
+     * @param reason the reason
+     */
     public MojException(String msg, Exception reason){
         super(msg, reason);
     }
 
+    /**
+     * Instantiates a new Moj exception.
+     *
+     * @param msg the msg
+     */
     public MojException(String msg){
         super(msg);
     }
