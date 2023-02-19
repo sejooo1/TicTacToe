471849e963713b0e839a7b7d9555d5061811741f
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:44:10 +0100

    JavaDoc za Idable!

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * The interface Idable used for forcing all classes to have id field.
+ */
 public interface Idable {
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     void setId(int id);
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     int getId();
 }
