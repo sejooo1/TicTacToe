d364be46daa65c42772a6473278b6184604c6fe5
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:49:21 +0100

    JavaDoc za DAO!

--- src/main/java/ba/unsa/etf/rpr/dao/DAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DAO.java
@@ -4,14 +4,52 @@ import ba.unsa.etf.rpr.exceptions.MojException;
 
 import java.util.List;
 
+/**
+ * The interface Dao.
+ *
+ * @param <T> the type parameter
+ */
 public interface DAO<T>{
+    /**
+     * Gets by id.
+     *
+     * @param id the id
+     * @return the by id
+     * @throws MojException the moj exception
+     */
     T getById(int id) throws MojException;
 
+    /**
+     * Add t.
+     *
+     * @param item the item
+     * @return the t
+     * @throws MojException the moj exception
+     */
     T add(T item) throws MojException;
 
+    /**
+     * Update t.
+     *
+     * @param item the item
+     * @return the t
+     * @throws MojException the moj exception
+     */
     T update(T item) throws MojException;
 
+    /**
+     * Delete.
+     *
+     * @param id the id
+     * @throws MojException the moj exception
+     */
     void delete(int id) throws MojException;
 
+    /**
+     * Gets all.
+     *
+     * @return the all
+     * @throws MojException the moj exception
+     */
     List<T> getAll() throws MojException;
 }
