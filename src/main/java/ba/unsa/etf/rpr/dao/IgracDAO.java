ecf414ce8d7469f3771f1903e7507b71cfbe9ad5
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:51:13 +0100

    JavaDoc za IgracDAO!

--- src/main/java/ba/unsa/etf/rpr/dao/IgracDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/IgracDAO.java
@@ -6,7 +6,17 @@ import ba.unsa.etf.rpr.exceptions.MojException;
 import java.util.List;
 
 
+/**
+ * DAO interface for Igrac JavaBean class.
+ */
 public interface IgracDAO extends DAO<Igrac>{
 
+    /**
+     * Daj po imenu list.
+     *
+     * @param ime the ime
+     * @return the list
+     * @throws MojException the moj exception
+     */
     List<Igrac> dajPoImenu(String ime) throws MojException;
 }
