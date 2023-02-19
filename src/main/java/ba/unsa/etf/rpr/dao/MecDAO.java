20d3c2df8617a40509c15fe9679758b4178bb90f
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:52:35 +0100

    JavaDoc za MecDao!

--- src/main/java/ba/unsa/etf/rpr/dao/MecDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MecDAO.java
@@ -6,7 +6,17 @@ import ba.unsa.etf.rpr.exceptions.MojException;
 
 import java.util.List;
 
+/**
+ * DAO interface for Mec JavaBean class.
+ */
 public interface MecDAO extends DAO<Mec>{
 
+    /**
+     * Daj pobjede igraca list.
+     *
+     * @param id the id
+     * @return the list
+     * @throws MojException the moj exception
+     */
     List<Mec> dajPobjedeIgraca(int id) throws MojException;
 }
