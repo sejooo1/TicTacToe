1347aa6c945f944c58b254f24742b0a5ecba7620
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-19 23:04:49 +0100

    Dodan ResourceBundle u OpcijeController!

--- src/main/java/ba/unsa/etf/rpr/controllers/OpcijeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OpcijeController.java
@@ -9,13 +9,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class OpcijeController {
 
     @FXML
     public void handleNazadClick(ActionEvent event) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"), ResourceBundle.getBundle("translation"));
             Parent root = (Parent) fxmlLoader.load();
             Scene newScene = new Scene(root);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
