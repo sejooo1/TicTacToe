d3315cac3f16ade9778cd6311e3a974a6373dc74
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:50:13 +0100

    JavaDoc za FactoryDAO!

--- src/main/java/ba/unsa/etf/rpr/dao/FactoryDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/FactoryDAO.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.dao;
 
+/**
+ * Factory method for singleton implementation of DAOs.
+ */
 public class FactoryDAO {
     private static final IgracDAO igracDAO = IgracDAOSql.getInstance();
     private static final MecDAO mecDAO = MecDAOSql.getInstance();
@@ -7,14 +10,29 @@ public class FactoryDAO {
     private FactoryDAO(){
     }
 
+    /**
+     * Igrac dao igrac dao.
+     *
+     * @return the igrac dao
+     */
     public static IgracDAO igracDAO(){
         return igracDAO;
     }
 
+    /**
+     * Mec dao mec dao.
+     *
+     * @return the mec dao
+     */
     public static MecDAO mecDAO(){
         return mecDAO;
     }
 
+    /**
+     * Tip dao tip dao.
+     *
+     * @return the tip dao
+     */
     public static TipDAO tipDAO(){
         return tipDAO;
     }
