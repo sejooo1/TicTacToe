fc1b7bef09f9bf08fe64b91278b6d7e90ca2cc24
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 00:46:44 +0100

    JavaDoc za Mec!

--- src/main/java/ba/unsa/etf/rpr/domain/Mec.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Mec.java
@@ -2,12 +2,23 @@ package ba.unsa.etf.rpr.domain;
 
 import java.util.Objects;
 
+/**
+ * JavaBean Class Mec.
+ */
 public class Mec implements Idable{
     private int idMeca;
     private int idX;
     private int idO;
     private int idTipa;
 
+    /**
+     * Instantiates a new Mec.
+     *
+     * @param idMeca the id meca
+     * @param idX    the id x
+     * @param idO    the id o
+     * @param idTipa the id tipa
+     */
     public Mec(int idMeca, int idX, int idO, int idTipa) {
         this.idMeca = idMeca;
         this.idX = idX;
@@ -15,6 +26,9 @@ public class Mec implements Idable{
         this.idTipa = idTipa;
     }
 
+    /**
+     * Instantiates a new Mec.
+     */
     public Mec() {
 
     }
@@ -27,26 +41,56 @@ public class Mec implements Idable{
         this.idMeca = idMeca;
     }
 
+    /**
+     * Gets id x.
+     *
+     * @return the id x
+     */
     public int getIdX() {
         return idX;
     }
 
+    /**
+     * Sets id x.
+     *
+     * @param idX the id x
+     */
     public void setIdX(int idX) {
         this.idX = idX;
     }
 
+    /**
+     * Gets id o.
+     *
+     * @return the id o
+     */
     public int getIdO() {
         return idO;
     }
 
+    /**
+     * Sets id o.
+     *
+     * @param idO the id o
+     */
     public void setIdO(int idO) {
         this.idO = idO;
     }
 
+    /**
+     * Gets id tipa.
+     *
+     * @return the id tipa
+     */
     public int getIdTipa() {
         return idTipa;
     }
 
+    /**
+     * Sets id tipa.
+     *
+     * @param idTipa the id tipa
+     */
     public void setIdTipa(int idTipa) {
         this.idTipa = idTipa;
     }
