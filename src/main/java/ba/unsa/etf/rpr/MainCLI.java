dfbcb19cb98e923ed2abca0a903d57ec23d4deb6
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 14:12:39 +0100

    Opcija nova igra!

--- src/main/java/ba/unsa/etf/rpr/MainCLI.java
+++ src/main/java/ba/unsa/etf/rpr/MainCLI.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import org.apache.commons.cli.Option;
+
 public class MainCLI {
+    private static final Option novaIgra = new Option("ni", "nova-igra", false, "Započinje novu igru!");
 }
