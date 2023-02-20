801a1f64b928bfab55b3b6e2b5d1cfa47ad7eda9
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 19:08:43 +0100

    Option statistika!

--- src/main/java/ba/unsa/etf/rpr/MainCLI.java
+++ src/main/java/ba/unsa/etf/rpr/MainCLI.java
@@ -5,4 +5,5 @@ import org.apache.commons.cli.Option;
 public class MainCLI {
     private static final Option novaIgra = new Option("ni", "nova-igra", false, "Započinje novu igru!");
     private static final Option statistika = new Option("s", "statistika", false, "Statistika igrača");
+    private static final Option dodajIgrace = new Option("di", "dodaj-igrace", false, "Dodaj nove igrače u bazu");
 }
