4dfef5613300b02f3c39e95ba3b13e68cdf86672
sejooo1 <sfejzagic1@etf.unsa.ba>
2023-02-20 19:11:17 +0100

    Dodan printFormattedOptions!

--- src/main/java/ba/unsa/etf/rpr/MainCLI.java
+++ src/main/java/ba/unsa/etf/rpr/MainCLI.java
@@ -1,9 +1,19 @@
 package ba.unsa.etf.rpr;
 
-import org.apache.commons.cli.Option;
+import org.apache.commons.cli.*;
+
+import java.io.PrintWriter;
 
 public class MainCLI {
     private static final Option novaIgra = new Option("ni", "nova-igra", false, "Započinje novu igru!");
     private static final Option statistika = new Option("s", "statistika", false, "Statistika igrača");
     private static final Option dodajIgrace = new Option("di", "dodaj-igrace", false, "Dodaj nove igrače u bazu");
+
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar TicTacToe.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
 }
